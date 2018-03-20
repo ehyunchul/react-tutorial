1b3ea57a2f353cad0650b4373e738aa57d39260f
HYUNCHUL <chul0830@gmail.com>
2018-03-20 12:09:49 +0900

    declaring a winner

--- src/index.js
+++ src/index.js
@@ -2,27 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-class Square extends React.Component {
-    // toggleValue(v) {
-    //     return this.state.value ? {value: null} : {value: v};
-    // }
-
-    // toggleValue2(v) {
-    //     if (this.state.value) {
-    //         this.setState({value: null})
-    //     } else {
-    //         this.setState({value: v})
-    //     }
-    // }
-
-    render() {
-        return (
-            <button className="square" onClick={() => this.props.onClick()}>
-            {/*<button className="square" name={this.props.value} onClick={() => this.toggleValue2('X')}>*/}
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Blocks extends React.Component {
@@ -46,20 +31,28 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: new Array(Math.pow(this.props.size, 2)).fill(null),
+            xIsNext: true,
         }
     }
 
     handleClick(i) {
-        console.log(i)
         const squares = this.state.squares.slice();
-        squares[i] = 'X'
-        this.setState({squares: squares})
+        if (calculateWinner(squares) || squares[i]) return;
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        })
+
+        calculateWinner(squares);
     }
 
     renderSquare(i) {
         return <Square value={this.state.squares[i]}
+                       index={i}
                        key={'square_'.concat(i)}
-                       onClick={() => this.handleClick(i)} />;
+                       onClick={() => this.handleClick(i)}/>;
     }
 
     createBlocks(size) {
@@ -71,13 +64,20 @@ class Board extends React.Component {
             for (let r = 0; r < size; r++) {
                 rows.push(this.renderSquare(count++));
             }
-            blocks.push(<Blocks blocks={rows} key={'board_'.concat(c.toString())} />)
+            blocks.push(<Blocks blocks={rows} key={'board_'.concat(c.toString())}/>)
         }
         return blocks;
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: '.concat(this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -94,7 +94,7 @@ class ExtensibleGame extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board size={this.props.size} />
+                    <Board size={this.props.size}/>
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
@@ -108,6 +108,84 @@ class ExtensibleGame extends React.Component {
 // ========================================
 
 ReactDOM.render(
-    <ExtensibleGame size={16}/>,
+    <ExtensibleGame size={4}/>,
     document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+    const size = Math.sqrt(squares.length);
+    let winner = null;
+
+    let horizonFill = function () {
+        for (let i = 0; i < Math.pow(size, 2) - size; i = i + size) {
+            let player = squares[i];
+            if (player && allEqualsValueInArray(player, () => squares.slice(i, i + size))) {
+                winner = player;
+            }
+        }
+    };
+
+    let verticalFill = function () {
+        for (let i = 0; i < size; i++) {
+            let player = squares[i];
+            if (player &&
+                allEqualsValueInArray(player,
+                    () => {
+                        let verticals = [player];
+                        let index = i;
+                        for (let j = 0; j < size; j++) {
+                            verticals.push(squares[index]);
+                            index += size;
+                        }
+                        return verticals;
+                    })) {
+                winner = player;
+            }
+        }
+    };
+
+    let crossFill = function () {
+        let player = squares[0];
+        let index = 0;
+
+        if (squares[0] &&
+            allEqualsValueInArray(player,
+                () => {
+                    let cross = [];
+                    for (let i = 0; i < size; i++) {
+                        cross.push(squares[index]);
+                        index += 1 + size;
+                    }
+                    return cross;
+                }
+            )) {
+            winner = player;
+        }
+
+        player = squares[size - 1];
+        index = size - 1;
+        if (squares[size - 1] &&
+            allEqualsValueInArray(player,
+                () => {
+                    let cross = [];
+                    for (let i = 0; i < size; i++) {
+                        cross.push(squares[index]);
+                        index = (size - 1) * (i + 2);
+                    }
+                    return cross;
+                }
+            )) {
+            winner = player;
+        }
+    };
+
+    let allEqualsValueInArray = function (value, supplier) {
+        return supplier().every(x => x === value);
+    };
+
+    horizonFill();
+    verticalFill();
+    crossFill();
+
+    return winner;
+}
